b25b65a309e353d99959db02790e2f74a7354433
clyt <clyt@itu.dk>
2021-11-19 22:10:37 +0100

    Added print stack

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"os"
+	"runtime"
 	logger "github.com/Blyth77/DISYS_MiniProject03/logger"
 ) 
 
 func main() {
+	defer printStack() // The defer makes the program wait for the method to return before terminating TGPL-book p. 143
+
 	logger.ClearLog()
 	logger.LogFileInit("main", 0)
 }
 
+
+// Function for printing the stack when a goroutine panics TGPL-book p. 151
+func printStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	os.Stdout.Write(buf[:n])
+}
